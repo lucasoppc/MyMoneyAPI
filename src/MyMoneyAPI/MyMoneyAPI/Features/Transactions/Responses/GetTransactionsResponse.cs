b6d8590d44d2da6a66a6257c4e1f3ffa464e5b91
Lucas Hernandez <matiasoppc@icloud.com>
2024-06-13 00:36:08 -0300

    fix dates in transaction response

--- src/MyMoneyAPI/MyMoneyAPI/Features/Transactions/Responses/GetTransactionsResponse.cs
+++ src/MyMoneyAPI/MyMoneyAPI/Features/Transactions/Responses/GetTransactionsResponse.cs
@@ -5,10 +5,32 @@ namespace MyMoneyAPI.Features.Transactions.Responses;
 public class GetTransactionsResponse
 {
     public int Total => Transactions.Count;
-    public List<Transaction> Transactions { get; }
+    public List<TransactionResponse> Transactions { get; }
 
     public GetTransactionsResponse(List<Transaction> transactions)
     {
-        Transactions = transactions ?? [];
+        Transactions = transactions.Select(t =>
+            new TransactionResponse
+            {
+                AccountId = t.accountId,
+                Amount = t.amount,
+                Currency = t.currency,
+                Date = DateTime.Parse(t.date),
+                Description = t.description,
+                Id = t.id,
+                UserId = t.userId
+            }
+        ).ToList();
     }
+}
+
+public class TransactionResponse
+{
+    public string Id { get; set; }
+    public string AccountId { get; set; }
+    public string UserId { get; set; }
+    public string Description { get; set; }
+    public string Currency { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
 }
